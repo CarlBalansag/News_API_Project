d0df4885bb6d896bd07407e443e381b35b991569
carl <carlgbt@gmail.com>
2023-08-28 19:43:48 -0700

    updated react project

--- src/App.js
+++ src/App.js
@@ -6,304 +6,54 @@ import React, { useState, useEffect } from 'react';
 
 function App( ) {
 
-  const [topic, setTopic] = useState('BreakingNews');
+  const [topic, setTopic] = useState('us');
+  const [articles, setArticles] = useState([]);
+  const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
+  console.log(topic)
 
-
-  function BreakingNews() {
-
-    
-    let [articlesData, setData] = useState(null);
-    const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.articles)
-          })
-          .catch(error => console.error(error));
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-      
-
-      fetchData();
-    }, []);
-
-    // console.log(articlesData)
-
-
-    return(
-      <>
-        {Object.keys(articlesData || {}).map(key =>{
-          const item = articlesData[key];
-
-          let img = item.urlToImage
-
-          return (
-            <Card
-              
-              title={item.title}
-              description={item.description}
-              image={img}
-              link={item.url}
-              key = {item.key}
-
-            />
-          )
-        })}
-      </>
-    )
-
-  }
-
-
-  function Sports() {
-
-    
-    let [articlesData, setData] = useState(null);
-    const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          fetch(`
-            https://newsapi.org/v2/everything?q=Sports&from=2023-08-21&to=2023-08-21&sortBy=popularity&apiKey=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.articles)
-          })
-          .catch(error => console.error(error));
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-      
-
-      fetchData();
-    }, []);
-
-    // console.log(articlesData)
-
-
-    return(
-      <>
-        {Object.keys(articlesData || {}).map(key =>{
-          const item = articlesData[key];
-
-          let img = item.urlToImage
-
-          return (
-            <Card
-              
-              title={item.title}
-              description={item.description}
-              image={img}
-              link={item.url}
-              key = {item.key}
-
-            />
-          )
-        })}
-      </>
-    )
-  }
-
-
-  function Politics() {
-
-    
-    let [articlesData, setData] = useState(null);
-    const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          fetch(`
-            https://newsapi.org/v2/everything?q=Politics&from=2023-08-21&to=2023-08-21&sortBy=popularity&apiKey=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.articles)
-          })
-          .catch(error => console.error(error));
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-      
-
-      fetchData();
-    }, []);
-
-    // console.log(articlesData)
-
-
-    return(
-      <>
-        {Object.keys(articlesData || {}).map(key =>{
-          const item = articlesData[key];
-
-          let img = item.urlToImage
-
-          return (
-            <Card
-              
-              title={item.title}
-              description={item.description}
-              image={img}
-              link={item.url}
-              key = {item.key}
-
-            />
-          )
-        })}
-      </>
-    )
-  }
-
-
-  function WorldNews() {
-
-    
-    let [articlesData, setData] = useState(null);
-    const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          fetch(`
-            https://newsapi.org/v2/everything?q=International&from=2023-08-21&to=2023-08-21&sortBy=popularity&apiKey=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.articles)
-          })
-          .catch(error => console.error(error));
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-      
-
-      fetchData();
-    }, []);
-
-    // console.log(articlesData)
-
-
-    return(
-      <>
-        {Object.keys(articlesData || {}).map(key =>{
-          const item = articlesData[key];
-
-          let img = item.urlToImage
-
-          return (
-            <Card
-              
-              title={item.title}
-              description={item.description}
-              image={img}
-              link={item.url}
-              key = {item.key}
-
-            />
-          )
-        })}
-      </>
-    )
-  }
-
-  function USNews() { 
-    
-    
-    let [articlesData, setData] = useState(null);
-    const apiKey = '32f6d727f00b4b939f2eb4299435f9a7'
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          fetch(`
-            https://newsapi.org/v2/everything?q=America&from=2023-08-21&to=2023-08-21&sortBy=popularity&apiKey=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.articles)
-          })
-          .catch(error => console.error(error));
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
-      
-
-      fetchData();
-    }, []);
-
-    // console.log(articlesData)
-
-
-    return(
-      <>
-        {Object.keys(articlesData || {}).map(key =>{
-          const item = articlesData[key];
-
-          let img = item.urlToImage
-
-          return (
-            <Card
-              
-              title={item.title}
-              description={item.description}
-              image={img}
-              link={item.url}
-              key = {item.key}
-
-            />
-          )
-        })}
-      </>
-    )
-  }
-
-  let type = ""
-
-  if (topic === "BreakingNews"){
-    type = BreakingNews();
-  } else if (topic === "Sports") {
-    type = Sports();
-  } else if (topic === "Politics") {
-    type = Politics();
-  } else if (topic === "USNews") {
-    type = USNews();
-  } else if ( topic === "WorldNews") {
-    type = WorldNews()
-  } else {
-    type = BreakingNews()
-    console.log("Error on the type")
-  }
-
-  console.log(topic);
+  useEffect(() => {
+    if(topic === 'sports') {
+      fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${topic}&apiKey=${apiKey}`)
+        .then(res => res.json())
+        .then(data => setArticles(data.articles))
+          .catch(err => console.log(err.message));
+    }
+    if(topic === 'us') {
+      fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
+        .catch(err => console.log(err.message));
+    }
+    if(topic === 'world') {
+      fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`)
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
+        .catch(err => console.log(err.message));
+    }
+    if(topic === 'politics') {
+      fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`)
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
+        .catch(err => console.log(err.message));
+    }
+    if(topic === 'entertainment') {
+      fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${topic}&apiKey=${apiKey}`)
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
+        .catch(err => console.log(err.message));
+    }
+  }, [topic])
 
   return (
-    <div className="App bg-slate-100	">
+    <>
       <Nav setTopic={setTopic} />
-
-      <main className="inline-grid grid-cols-5 gap-12">
-
-      {type}
-        
-      </main>
-
-    </div>
-  );
+      <div className='grid grid-cols-6 gap-6'>
+      {articles?.map(item => {
+        return <Card title={item.title} description={item.description} image={item.urlToImage} link={item.url} key={item.title} />
+      })}
+      </div>
+    </>
+  )
 }
 
 export default App;
-
-      {/* {topic === "BreakingNews" && BreakingNews()}
-      {topic === "Sports" && Sports()}
-      {topic === "Politics" && Politics()}
-      {topic === "USNews" && USNews()}
-      {topic === "WorldNews" && WorldNews()} */}
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -13,13 +13,13 @@ return (
 
     <nav className="mb-12 bg-slate-100 font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
         <div className="mb-2 sm:mb-0">
-            <button onClick={()=>onButtonClick('BreakingNews')} className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">News API</button>
+            <button onClick={()=>onButtonClick('us')} className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">News API</button>
         </div>
         <div>
-            <button onClick={()=>onButtonClick('Sports')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">Sports</button>
-            <button onClick={()=>onButtonClick('Politics')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">Politics</button>
-            <button onClick={()=>onButtonClick('USNews')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">US News</button>
-            <button onClick={()=>onButtonClick('WorldNews')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">World News</button>
+            <button onClick={()=>onButtonClick('sports')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">Sports</button>
+            <button onClick={()=>onButtonClick('politics')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">Politics</button>
+            <button onClick={()=>onButtonClick('entertainment')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">Entertainment</button>
+            <button onClick={()=>onButtonClick('world')} className="text-lg pr-6 no-underline text-grey-darkest hover:text-blue-dark ml-2">World News</button>
         </div>
     </nav>
 
@@ -27,3 +27,4 @@ return (
 }
 
 export default Nav;
+
